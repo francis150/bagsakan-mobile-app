747de97483d93051b80b9de3970628895e3a57b4
Francis Dela Victoria <francisdelavictoria150@gmail.com>
2022-10-01 17:22:38 +0800

    Loader Modal

--- src/components/Modals/LoadingModal.js
+++ src/components/Modals/LoadingModal.js
@@ -1,14 +1,47 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { StyleSheet, Text, View, Modal, ActivityIndicator } from 'react-native'
 
-const LoadingModal = () => {
+import { Colors, Fonts } from '../../constants/Values'
+
+const LoadingModal = ({visible}) => {
   return (
-    <View>
-      <Text>LoadingModal</Text>
-    </View>
+    <Modal
+    visible={visible}
+    transparent={true}
+    animationType={'fade'}>
+      <View style={styles.backgroundOverlay}>
+        <View style={styles.contentContainer}>
+          <ActivityIndicator
+          animating={visible}
+          color={Colors.accentColor}
+          size={50} />
+          <Text
+          style={styles.text} >Just a second...</Text>
+        </View>
+      </View>
+    </Modal>
   )
 }
 
 export default LoadingModal
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  backgroundOverlay: {
+    flex: 1,
+    backgroundColor: Colors.modalOverlayBackground,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  contentContainer: {
+    backgroundColor: Colors.defaultWhite,
+    padding: 40,
+    borderRadius: 10,
+    alignItems: 'center'
+  },
+  text: {
+    marginTop: 20,
+    color: Colors.defaultBlack,
+    fontFamily: Fonts.regular,
+    fontSize: 16
+  }
+})
--- src/screens/AuthStack/LoginScreen.js
+++ src/screens/AuthStack/LoginScreen.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, TouchableWithoutFeedback, View } from 'react-native'
 
 import {Colors, Fonts, Layout} from '../../constants/Values'
 import { StackHeader } from '../../components/Headers'
-import {OTPVerificationModal, MessageModal} from '../../components/Modals'
+import {OTPVerificationModal, MessageModal, LoadingModal} from '../../components/Modals'
 import {PrimaryButton} from '../../components/Buttons'
 
 const LoginScreen = () => {
@@ -19,11 +19,7 @@ const LoginScreen = () => {
         <View
         style={styles.container} >
 
-            <MessageModal
-            visible={true}
-            title={'No internet connection 😱'}
-            message={'It looks like you have a slow or no internet connection. Please check your internet connection and try again.'}
-            onOkay={() => console.log('Okay')} />
+            
 
         </View>
     </TouchableWithoutFeedback>
